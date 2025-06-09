04f1ccf917febb8c4a351269e0259d6e2a9e3e53
Miradeljan <miradel51@users.noreply.github.com>
2025-06-09 14:42:25 +0800

    Update org-locations.js
    revised org-locations.js info

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -24,15 +24,80 @@ var addressPoints = [
 */
 
 var addressPoints = [
-  ["Westlake University, Hangzhou (AI for Medicine)", 30.17727, 120.09332],           // 杭州，浙江，中国
-  ["Changji University, Changji (LLM+Tool Learning)", 44.018556, 87.254485‌‌],       // 昌吉，新疆，中国
-  ["Yili Normal University, Yili (LLM+MT)", 43.937285, 81.305231‌‌],      // 伊犁，新疆，中国
-  ["CAS (Xinjiang Branch), Urumqi (CCL2023 Feelings)", 43.84039, 87.57307],      // 中国科学院新疆理化所（乌鲁木齐，新疆，中国）
-  ["Seattle, WA, USA (NAACL2022)", 47.60621, -122.33207],      // Seattle（Washington, US）
-  ["Damo Academy, Alibaba Group, Hangzhou (EMNLP2021 Feelings)", 30.29400, 120.01530],      // 阿里巴巴达摩院（杭州，浙江，中国）
-  ["Punta Cana, Dominican Republic (EMNLP2021 Oral)", 18.58182, -68.40431],      // Punta Cana（Dominican Republic）
-  ["Tsinghua University, Beijing (Ph.D. Forum)", 40.00229, 116.32099],      // 清华大学（北京，中国）
-  ["Minzu University of China, Beijing (LRLs+NMT)", 39.91667, 116.38333],      // 中央民族大学（北京，中国）
-  ["Peking University, Beijing (ICIS2018)", 39.91667, 116.38333],      // 北京大学（北京，中国）
-  ["Xinjiang University, Urumqi (Domain Adaptation)", 43.79840, 87.60387],      // 新疆大学（乌鲁木齐，新疆，中国）
+  // 杭州，浙江，中国
+  [
+    "Westlake University, Hangzhou (AI for Medicine)",
+    30.17727,
+    120.09332
+  ],
+
+  // 昌吉，新疆，中国
+  [
+    "Changji University, Changji (LLM+Tool Learning)",
+    44.018556,
+    87.254485‌‌
+  ],
+
+  // 伊犁，新疆，中国
+  [
+    "Yili Normal University, Yili (LLM+MT)",
+    43.937285,
+    81.305231‌‌
+  ],
+
+  // 中国科学院新疆理化所（乌鲁木齐，新疆，中国）
+  [
+    "CAS (Xinjiang Branch), Urumqi (CCL2023 Feelings)",
+    43.84039,
+    87.57307
+  ],
+
+  // Seattle（Washington, US）
+  [
+    "Seattle, WA, USA (NAACL2022)",
+    47.60621,
+    -122.33207
+  ],
+
+  // 阿里巴巴达摩院（杭州，浙江，中国）
+  [
+    "Damo Academy, Alibaba Group,Hangzhou (EMNLP2021 Feelings)",
+    30.29400,
+    120.01530
+  ],
+
+  // Punta Cana（Dominican Republic）
+  [
+    "Punta Cana, Dominican Republic (EMNLP2021 Oral)",
+    18.58182,
+    -68.40431
+  ],
+
+  // 清华大学（北京，中国）
+  [
+    "Tsinghua University, Beijing (Ph.D. Forum)",
+    40.00229,
+    116.32099
+  ],
+
+  // 中央民族大学（北京，中国）
+  [
+    "Minzu University of China, Beijing (LRLs+NMT)",
+    39.91667,
+    116.38333
+  ],
+
+  // 北京大学（北京，中国）
+  [
+    "Peking University, Beijing (ICIS2018)",
+    39.91667,
+    116.38333
+  ],
+
+  // 新疆大学（乌鲁木齐，新疆，中国）
+  [
+    "Xinjiang University, Urumqi (Domain Adaptation)",
+    43.79840,
+    87.60387
+  ] 
 ];
